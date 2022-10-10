0eab749572ddb10071de11640d6ffc54f45112ad
SSAnastasia <106982689+SSAnastasia@users.noreply.github.com>
2022-10-10 12:08:42 +0300

    update forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,7 +8,8 @@ export default function WeatherForecast(props) {
 
   useEffect(() => {
     setLoaded(false);
-  }, [props.coordinates]);
+  },
+   [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -26,6 +27,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            }else{
+              return null;
             }
           })}
         </div>
